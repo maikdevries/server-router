ae52d591c78a7a8db10d79956b5025cb0681dcd3
maikdevries <dev.maik.devries@outlook.com>
2025-07-29 16:35:03 +0200

    feat: support router-wide shared context through generics

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,25 @@
-export interface Context {
+type Context<C> = C & {
 	'params': URLPatternResult;
 	'url': URL;
-}
+};
 
 type BaseHandler = (request: Request) => Response | Promise<Response>;
-type Handler = (request: Request, context: Context) => Response | Promise<Response>;
+type Handler<C> = (request: Request, context: Context<C>) => Response | Promise<Response>;
 
-export interface Route {
-	'handler': Handler;
+export interface Route<C> {
+	'handler': Handler<C>;
 	'method': string[];
 	'pattern': URLPattern;
 }
 
-export default function route(routes: Route[], fallback: BaseHandler): BaseHandler {
+export default function route<C>(context: C, routes: Route<C>[], fallback: BaseHandler): BaseHandler {
 	return (request: Request) => {
 		for (const route of routes) {
 			const match = route.pattern.exec(request.url);
 
 			if (match && route.method.includes(request.method)) {
 				return route.handler(request, {
+					...context,
 					'params': match,
 					'url': new URL(request.url),
 				});
