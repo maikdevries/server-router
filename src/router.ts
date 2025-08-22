3cc64915c6946ad1701bec64fef1df7660f1d915
maikdevries <dev.maik.devries@outlook.com>
2025-08-22 16:16:07 +0200

    feat: use empty context as default for generic type parameter

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import type { Override } from './types.ts';
+import type { Empty, Override } from './types.ts';
 
 type Method = 'CONNECT' | 'DELETE' | 'GET' | 'HEAD' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT' | 'TRACE';
 
@@ -7,15 +7,15 @@ export type RouteContext<C> = Override<C, {
 	'url': URL;
 }>;
 
-export type Handler<C> = (request: Request, context: C) => Response | Promise<Response>;
+export type Handler<C = Empty> = (request: Request, context: C) => Response | Promise<Response>;
 
-export interface Route<C> {
+export interface Route<C = Empty> {
 	'handler': Handler<RouteContext<C>>;
 	'method': Method[];
 	'pattern': URLPattern;
 }
 
-export function route<C>(routes: Route<C>[], fallback: Handler<C>): Handler<C> {
+export function route<C = Empty>(routes: Route<C>[], fallback: Handler<C>): Handler<C> {
 	return (request: Request, context: C) => {
 		for (const route of routes) {
 			const match = route.pattern.exec(request.url);
