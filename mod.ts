2425778cb4cc3bdc8888934fad25be6b22e1539e
maikdevries <dev.maik.devries@outlook.com>
2026-01-17 14:01:04 +0100

    feat: extract middleware into separate repository

--- mod.ts
+++ mod.ts
@@ -1,6 +1,3 @@
-export type { Middleware } from './src/middleware.ts';
-export { chain } from './src/middleware.ts';
-
 export type { Handler, Route, RouteContext } from './src/router.ts';
 export { route } from './src/router.ts';
 
--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,3 @@
 export type Empty = Record<never, never>;
 
-type Common<A, B> = {
-	[K in keyof A & keyof B]: B[K] extends never ? A[K] : B[K];
-};
-
-type Distinct<A, B> = {
-	[K in keyof Omit<A, keyof B>]: A[K];
-};
-
-export type Merge<A, B> = Common<A, B> & Distinct<A, B> & Distinct<B, A>;
-
 export type Override<T, S> = Omit<T, keyof S> & S;
-
-type CommonKeys<A, B> = {
-	[K in keyof A & keyof B]: A[K] extends B[K] ? K : never;
-}[keyof A & keyof B];
-
-export type Reduce<A, B, C> = A & Omit<B, CommonKeys<C, B>>;
