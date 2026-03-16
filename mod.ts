47076f5dcbdfabd64e0841ee152656c7c7978b1d
maikdevries <dev.maik.devries@outlook.com>
2026-03-16 09:33:28 +0100

    docs: write documentation for module

--- mod.ts
+++ mod.ts
@@ -1,4 +1,31 @@
-export type { Handler, Route, RouteContext } from './src/router.ts';
-export { route } from './src/router.ts';
-
-export type { Empty } from './src/types.ts';
+/**
+ * This module provides a flexible approach to routing incoming HTTP requests to handlers in a type-safe manner. Each
+ * route defines the request properties it matches on and the request handler it dispatches to. Handlers receive
+ * supplemental context data merged with route-specific properties. A fallback handler covers requests that do not match
+ * a registered route. The type system ensures at compile-time that all required context properties are provided.
+ *
+ * @example Basic usage
+ * ```ts
+ * import { route, type RouteContext } from '@maikdevries/server-router';
+ *
+ * interface Context {
+ * 	'uuid': string;
+ * }
+ *
+ * const router = route<Context>(
+ * 	[
+ * 		{
+ * 			'method': ['GET'],
+ * 			'pattern': new URLPattern({ 'pathname': '/uuid' }),
+ * 			'handler': (request, context: RouteContext<Context>) => Response.json({ 'uuid': context.uuid }),
+ * 		},
+ * 	],
+ * 	() => new Response('404 Not Found', { 'status': 404 }),
+ * );
+ *
+ * const app = (request: Request) => router(request, { 'uuid': self.crypto.randomUUID() });
+ * ```
+ *
+ * @module
+ */
+export { route, type RouteContext } from '@self/core';
